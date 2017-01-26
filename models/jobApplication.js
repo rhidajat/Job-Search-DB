539e38030369021af6b49213780a5853238df6ca
rheza <rhezahidajat@gmail.com>
2017-01-25 17:16:44 -0800

    job application schema

--- models/jobApplication.js
+++ models/jobApplication.js
@@ -1,6 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var JobApplication = new Schema({
-
+var statusSchema = new Schema({
+    date: Date,
+    status: {
+        type: String,
+        enum: ['Started Application', 'Submitted Application', 'Rejected', 'Received Interests',
+            'Initial Call', 'Phone Screen', 'Onsite Interview', 'Received Job Offer',
+            'Accepted Job Offer', 'Declined Job Offer']
+    },
+    contactPerson: String,
+    comment: String
 });
+
+var JobApplicationSchema = new Schema({
+    companyName: String,
+    jobTitle: String,
+    city: String,
+    region: String,
+    country: String,
+    comment: String,
+    progress: [statusSchema]
+});
+
+module.exports = mongoose.model('JobApplication', JobApplicationSchema);
